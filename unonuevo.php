73301245840429d491dfcce8170ae8393c1cbfd6
naaness <nestor.andres.a@gmail.com>
2013-10-08 15:29:18 -0500

    Update unonuevo.php

--- unonuevo.php
+++ unonuevo.php
@@ -1,5 +1,27 @@
 <?php 
 
 echo "Andrés es una huevinha!";
+echo NumeroPrimo(15)
 
+function NumeroPrimo($Num)
+{
+  $con=0
+  $conmul=0
+  while($con<=$num)
+  {
+    if ($num % $con == 0)
+    {
+      $conmul+=1
+    }
+    $con+=1
+  }
+  if($conmul>2)
+  {
+    return $Num + " no es una numero primo"
+  }
+  else
+  {
+    return $Num + " es una numero primo"
+  }
+}
 ?>
